1329bf25791a352376920854e38684714871a3c4
Rahul Koli <111896957+Rahulbeginner1431@users.noreply.github.com>
2023-10-12 17:24:59 +0000

    Conversion DataTypes

---
+++ basic/conversion.js
@@ -0,0 +1,28 @@
+//let score = "33aa"
+//console.log(typeof score)
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+/*
+let boolean = 1;
+
+let booleanIsLoggedIn = Boolean(boolean)  // there are type conversesion 
+                                        // Ex - string to number , number to string , 1 to boolean
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 =>false;
+// "" => false
+// "Rahul" => true
+
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
